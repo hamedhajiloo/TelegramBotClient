05c66aa6ae63834117c8784a6751ef16dc6c6590
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-07-10 02:23:36 +0430

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static string token = "1852172683:AAGDoHdb_YjI7KqncQNuPKoG2CT4MeQz_tA";
+        public static int lastUpdateId = 0;
+        static async Task Main(string[] args)
+        {
+            var telegramBot = new TelegramBotClient(token);
+            //await telegramBot.SendMessageAsync("120714541", "test from vafs!");
+
+            while (true)
+            {
+                var res = await telegramBot.GetUpdatesAsync(lastUpdateId + 1);
+                if (res.Any())
+                {
+                    lastUpdateId = res.Last().UpdateId;
+
+                    foreach (var message in res)
+                    {
+                        if (message.Text == "سلام")
+                        {
+                            await telegramBot.SendMessageAsync(message.SenderChatId, "علیک");
+                        }
+                        else if (message.Text == "هوی")
+                        {
+                            await telegramBot.SendMessageAsync(message.SenderChatId, "های");
+                        }
+                        else
+                        {
+                            var sendMessage = $"{message.FullName} says {message.Text}";
+                            await telegramBot.SendMessageAsync(message.SenderChatId, sendMessage);
+                        }
+                    }
+
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
---
+++ TelegramBot/TelegramBotClient.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    public class TelegramBotClient
+    {
+        private readonly string _token;
+        private readonly HttpClient client;
+
+        public TelegramBotClient(string token)
+        {
+            _token = token ?? throw new ArgumentNullException(nameof(token));
+            client = new HttpClient();
+        }
+
+        public async Task SendMessageAsync(string chatId, string text)
+        {
+            var url = $"https://api.telegram.org/bot{_token}/sendMessage";
+
+            var message = new
+            {
+                chat_id = chatId,
+                text = text
+            };
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json")
+            };
+
+            await client.SendAsync(httpRequest);
+        }
+
+        public async Task<List<Message>> GetUpdatesAsync(int offset)
+        {
+            var updates = new List<Message>();
+            var url = $"https://api.telegram.org/bot{_token}/getUpdates";
+
+            var message = new
+            {
+                offset = offset,
+            };
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json")
+            };
+
+            var result = await client.SendAsync(httpRequest);
+
+            var response = await result.Content.ReadAsStringAsync();
+            JObject json = JObject.Parse(response);
+
+            var status = json["ok"].ToString();
+            if (!bool.Parse(status)) return updates;
+
+            var resultUpdates = json["result"].ToList();
+
+            foreach (var resultUpdate in resultUpdates)
+            {
+                var messageJson = resultUpdate["message"];
+                var message1 = new Message
+                {
+                    UpdateId = int.Parse(resultUpdate["update_id"].ToString()),
+                    Id = int.Parse(messageJson["message_id"].ToString()),
+                    Text = messageJson["text"].ToString(),
+                    SenderChatId = messageJson["from"]["id"].ToString(),
+                    FullName = $"{messageJson["from"]["first_name"]} {messageJson["from"]["last_name"]}"
+                };
+
+                updates.Add(message1);
+            }
+
+            return updates;
+        }
+
+        
+        public class Message
+        {
+            public int UpdateId { get; set; }
+            public int Id { get; set; }
+            public string Text { get; set; }
+            public string SenderChatId { get; set; }
+            public string FullName { get; set; }
+        }
+    }
+}
